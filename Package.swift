7c37c50deca52ed9b22f4bcbb86f6c4eb3ddd28e
zxss702 <zxs20060621@icloud.com>
2026-02-07 01:33:44 +0800

    chore: bump platform requirements to iOS 18 / macOS 15 / Swift 6.0
    - Update Swift tools version from 5.9 to 6.0
    - Raise minimum iOS version from 16 to 18
    - Raise minimum macOS version from 13 to 15
    - Add .ShengYan Code Edit/snapshot/ to .gitignore

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.9
+// swift-tools-version: 6.0
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -6,8 +6,8 @@ import PackageDescription
 let package = Package(
     name: "MarkdownExtendedView",
     platforms: [
-        .iOS(.v16),
-        .macOS(.v13)
+        .iOS(.v18),
+        .macOS(.v15)
     ],
     products: [
         .library(
